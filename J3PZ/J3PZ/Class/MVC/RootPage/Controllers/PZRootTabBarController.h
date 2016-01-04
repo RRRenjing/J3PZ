a006385ed9692b9e01f2f777aa5e77787a437f50
RRRenj <rrrenj@outlook.com>
2016-01-04 14:59:05 +0800

    文件框架

---
+++ J3PZ/J3PZ/Class/MVC/RootPage/Controllers/PZRootTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  PZRootTabBarController.h
+//  J3PZ
+//
+//  Created by 千锋 on 16/1/4.
+//  Copyright © 2016年 1111111111. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PZRootTabBarController : UITabBarController
+
+@end
