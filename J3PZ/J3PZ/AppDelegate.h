869fa74bd44fb8fa4a7c5d833382c06a660f66cb
RRRenj <rrrenj@outlook.com>
2016-01-04 14:42:46 +0800

    文件夹创建

---
+++ J3PZ/J3PZ/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  J3PZ
+//
+//  Created by 千锋 on 16/1/4.
+//  Copyright © 2016年 1111111111. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ J3PZ/J3PZTests/J3PZTests.m
@@ -0,0 +1,39 @@
+//
+//  J3PZTests.m
+//  J3PZTests
+//
+//  Created by 千锋 on 16/1/4.
+//  Copyright © 2016年 1111111111. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface J3PZTests : XCTestCase
+
+@end
+
+@implementation J3PZTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
